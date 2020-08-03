49e952b9c9e270e8c6511bcd7adf0be339c0c5ec
ender-project <69098385+ender-project@users.noreply.github.com>
2020-08-03 20:11:10 +0900

    Add files via upload

--- getstatus.js
+++ getstatus.js
@@ -4,7 +4,6 @@ xhr.responseType = 'json';
 xhr.send();
 const response1 = xhr.response;
 const response = JSON.parse(response1);
-const ping = String(response.ping);
 document.open();
-document.write(ping)
+document.write(response)
 document.close();
