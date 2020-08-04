e8d1b803f8e94f5b74a941ff863696cd8ac26c41
ender-project <69098385+ender-project@users.noreply.github.com>
2020-08-04 23:45:23 +0900

    Add files via upload

--- getstatus.js
+++ getstatus.js
@@ -1,5 +1,5 @@
 $.getJSON(
-    "https://status.ender-bot.ml", //リクエストURL
+    "https://status.ender-bot.ml/get.json", //リクエストURL
     null,       //送信データ
     function(data, status) {
       // 通信成功時の処理
