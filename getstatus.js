af0f511090130915da8a37896a2eddabf09e1a68
ender-project <69098385+ender-project@users.noreply.github.com>
2020-08-05 00:00:48 +0900

    Delete getstatus.js

--- getstatus.js
+++
@@ -1,6 +0,0 @@
-$.getJSON("//ender-bot.ml/get.json", function() {
-    alert("success");
-  })
-  .success(function() { alert("second success"); })
-  .error(function() { alert("error"); })
-  .complete(function() { alert("complete"); });
